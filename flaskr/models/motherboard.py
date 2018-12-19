57fbd2db647de804963a00c645b32becd88c2063
Dima Lukashov <lds4ever2000@gmail.com>
2018-12-19 22:59:11 +0200

    bugfix/fix-migrations Fix migrations

--- flaskr/models/motherboard.py
+++ flaskr/models/motherboard.py
@@ -5,10 +5,10 @@ from .base import ComponentMixin
 class Motherboard(db.Model, ComponentMixin):
     form_factor = db.Column(db.String(10), nullable=False)
     chipset = db.Column(db.String(10), nullable=False)
-    pci_slots = db.Column(db.Integer, nullable=False)
-    used_pci_slots = db.Column(db.Integer, nullable=False)
-    ram_slots = db.Column(db.Integer, nullable=False)
-    used_ram_slots = db.Column(db.Integer, nullable=False)
+    pci_slots = db.Column(db.Integer, nullable=False, default=0)
+    used_pci_slots = db.Column(db.Integer, nullable=False, default=0)
+    ram_slots = db.Column(db.Integer, nullable=False, default=0)
+    used_ram_slots = db.Column(db.Integer, nullable=False, default=0)
 
     @property
     def free_pci_slots(self):
